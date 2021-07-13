579d1ab5b6eb07df43b2b277a0550d0ea8c1a1ba
Lakshay Kumar <lakshay.hex@gmail.com>
2021-07-13 19:58:50 +0530

    commit 4

--- app/src/main/java/com/hex/facedetectiontest/FaceOverlay.java
+++ app/src/main/java/com/hex/facedetectiontest/FaceOverlay.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FaceOverlay extends View {
-    public float top_space, overlay_height,overlay_width;
+    public float top_space;
+    public static float canvasHeight,canvasWidth;
+    public static float omx, omnx,omy,omny;
+    public static float topvalue=-1.0f;
     public FaceOverlay(Context context) {
         super(context);
         initView();
@@ -44,14 +49,7 @@ public class FaceOverlay extends View {
     }
 
     public void initView(){
-//        DisplayMetrics displayMetrics = new DisplayMetrics();
-//        getContext().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-//        int height = displayMetrics.heightPixels;
-//        int width = displayMetrics.widthPixels;
-//
-//        this.he
-
-
+        this.setLayerType(View.LAYER_TYPE_HARDWARE,null);
     }
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -62,22 +60,57 @@ public class FaceOverlay extends View {
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
 
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setAlpha(80);
-        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+        // paint = new Paint();
+        //paint.setColor(Color.BLACK);
+        //.setAlpha(95);
+        //canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+        canvasHeight = canvas.getHeight();
+        canvasWidth = canvas.getWidth();
+        top_space = 0.1f*canvas.getHeight();
 
-        top_space = 0.2f*canvas.getHeight();
-        overlay_height = 0.5f*canvas.getWidth();
-        overlay_width = 0.25f*canvas.getWidth();
-        float cx = canvas.getWidth()/2;
-        float cy = top_space+(overlay_height/2);
-        float radius = overlay_width/2;
 
-        Paint paintt = new Paint();
-        paintt.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        paintt.setColor(Color.TRANSPARENT);
-        canvas.drawCircle(cx, cy, radius, paintt);
+        float heightRad,widthRadius;
+        heightRad = 0.3f*canvasWidth;
+        widthRadius = 0.2f*canvasWidth;
+
+        Paint eraser = new Paint();
+        eraser.setAntiAlias(true);
+        eraser.setColor(Color.TRANSPARENT);
+       // eraser.setMaskFilter(null);
+//        eraser.setAlpha(0)  ;
+
+        eraser.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+
+
+
+
+        float right,left,top,bottom;
+        left = (canvasWidth-(2*widthRadius))/2;
+        right = left+(2*widthRadius);
+        top = top_space;
+        bottom = top+(2*heightRad);
+
+        RectF rect = new RectF(left,top,right,bottom);
+
+        float[] corners = new float[]{
+
+                widthRadius*2f, widthRadius*2f,          // Bottom right radius in px
+                widthRadius*2f, widthRadius*2f,
+                widthRadius*1.9f, heightRad*2.2f,          // Bottom right radius in px
+                widthRadius*1.9f, heightRad*2.2f
+        };
+
+        final Path path = new Path();
+        path.addRoundRect(rect, corners, Path.Direction.CW);
+        canvas.drawPath(path, eraser);
+
+        float padding = widthRadius*0.2f;
+        omnx = left - padding;
+        omny = top - padding;
+        omx = right+padding;
+        omy = bottom+padding;
+        topvalue = top_space+(heightRad);
+
 
     }
 
--- app/src/main/java/com/hex/facedetectiontest/MainActivity.java
+++ app/src/main/java/com/hex/facedetectiontest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.hex.facedetectiontest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import io.fotoapparat.Fotoapparat;
 import io.fotoapparat.characteristic.LensPosition;
 import io.fotoapparat.facedetector.FaceDetector;
@@ -16,14 +18,17 @@ import io.fotoapparat.view.CameraView;
 import android.content.Intent;
 import android.graphics.ImageFormat;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.os.Trace;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,12 +47,12 @@ public class MainActivity extends AppCompatActivity {
     FaceDetector faceDetector;
     RectanglesView rectanglesView;
     Fotoapparat f;
-    TextView co;
-    public int rectheight = ScannerOverlay.rectHeight;
-    public int rectwidth = ScannerOverlay.rectWidth;
-    TextView face_not_detecting;
+
+
     LottieAnimationView captured;
     String y="";
+    float omx, omnx,omy,omny,fmx,fmny,fmy,fmnx;
+
 
 
     @Override
@@ -57,19 +62,11 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         rectanglesView = findViewById(R.id.rectanglesView);
         CameraView cameraView = findViewById(R.id.cameraView);
-        co = findViewById(R.id.co);
-        face_not_detecting = findViewById(R.id.face_not_detecting);
+
         captured = findViewById(R.id.captured);
 
-        face_not_detecting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,Face_Detection_2.class));
-                //finish();
-            }
-        });
 
-
+        setHeightMargin();
 
         faceDetector = FaceDetector.create(this);
 
@@ -83,14 +80,6 @@ public class MainActivity extends AppCompatActivity {
                 .build()
                 .start();
 
-        findViewById(R.id.cap).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String path = Environment.getExternalStorageState()+"/"+System.currentTimeMillis()+"_img.jpg";
-                File file = new File(path);
-                f.takePicture();
-            }
-        });
     }
 
     private class CustomFrameProcessor implements FrameProcessor {
@@ -99,85 +88,42 @@ public class MainActivity extends AppCompatActivity {
 
 
             List<Rectangle> faces = faceDetector.detectFaces(frame.getImage(), frame.getSize().width, frame.getSize().height, frame.getRotation());
-            try{
-               y = faces.get(0).getY()+"";
 
-            }
-            catch (Exception e){
-               y = "";
+            omnx = FaceOverlay.omnx;
+            omx = FaceOverlay.omx;
+            omy = FaceOverlay.omy;
+            omny = FaceOverlay.omny;
+            Rectangle rectangle = new Rectangle(omnx/rectanglesView.getWidth(),omny/rectanglesView.getHeight(),(omx-omnx)/rectanglesView.getWidth(),(omy-omny)/rectanglesView.getHeight());
+            faces.add(rectangle);
 
-            }
-
-//            com.google.android.gms.vision.Frame f = new com.google.android.gms.vision.Frame.Builder().setImageData(ByteBuffer.wrap(frame.getImage()), frame.getSize().width, frame.getSize().height, ImageFormat.NV21).build();
-//            detectFaces(f);
-
-//            runOnUiThread(() -> rectanglesView.setRectangles(faces)
-//
-//            );
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    rectanglesView.setRectangles(faces);
+            if(!(faces.size() ==0)){
+                for(int i = 0;i<faces.size();i++){
                     try{
 
+                        final int left = (int) (faces.get(i).getX() * rectanglesView.getWidth());
+                        final int top = (int) (faces.get(i).getY() * rectanglesView.getHeight());
+                        final int right = left + (int) (faces.get(i).getWidth() * rectanglesView.getWidth());
+                        final int bottom = top + (int) (faces.get(i).getHeight() * rectanglesView.getHeight());
+                        fmnx = left;
+                        fmx = right;
+                        fmy = bottom;
+                        fmny=top;
 
-                        if(y.trim()!=""){
-                            captured.setVisibility(View.VISIBLE);
-                            captured.loop(false);
+
+                        boolean isinside = (fmnx>=omnx)&&(fmny>=omny)&&(fmx<=omx)&&(fmy<=omy);
+                        if(isinside==true){
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    onFaceScanned();
+                                }
+                            });
 
                         }
                         else {
-                           // captured.setVisibility(View.INVISIBLE);
-                           // captured.loop(true);
+
                         }
-
-
-
-
-
-                    float x = faces.get(0).getX();
-                    float y = faces.get(0).getY();
-                    float x_cent = (x + frame.getSize().width) / 2;
-                    float y_cent = (y + frame.getSize().height) / 2;
-                    DisplayMetrics displayMetrics = new DisplayMetrics();
-                    getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-                    int height = displayMetrics.heightPixels;
-                    int width = displayMetrics.widthPixels;
-
-                    Log.i("CamH/W"," height = "+height/2+" width="+width/2);
-                    Log.i("Co-ordinates","x = " + x + " Y = " + y + " x-cent = " + x_cent + " y-cent " + y_cent);
-                    co.setText((width/2)+" x_cent = "+x_cent+"x = "+x);
-//
-//                    if((int)x_cent< && (int)y_cent<(height/2)){
-//                        Log.i("Success---------->","Co-ordinates matched");
-//                        co.setText("matched");
-//                        Toast.makeText(getApplicationContext(),"Face match",Toast.LENGTH_SHORT).show();
-//                    }
-//                    else{
-//                        co.setText("Waiting for match");
-//                    }
-//
-//
-//
-//                    float x1=0.0f,x2=0.0f,y1=0.0f,y2=0.0f;
-//                    y1 = (float) ((height/2)-(0.25*(height/2)));
-//                    y2 = (float) ((height/2)+(0.25*(height/2)));
-//                    x1 = (float) ((width/2)-(0.25*(width/2)));
-//                    x2 = (float) ((width/2)+(0.25*(width/2)));
-//
-//                    if(x_cent>x1 && x_cent<x2&&y_cent>y1&&y_cent<y2){
-//
-//                    }
-//
-//                int[] location = new int[2];
-//                    View cent = findViewById(R.id.cent);
-//                    cent.getLocationOnScreen(location);
-//                    Log.e("Center co-ordinates","X axis is "+location[0] +"and Y axis is "+location[1]);
-//                co.setText("X axis is "+location[0] +"and Y axis is "+location[1]);
-//
-//
-
-
                     }
                     catch (Exception e){
 
@@ -185,48 +131,54 @@ public class MainActivity extends AppCompatActivity {
 
 
                     }
-
                 }
-            });
 
+            }
 
-
-
-
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    rectanglesView.setRectangles(faces);
+//                }
+//            });
 
         }
     }
+    void setHeightMargin(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (FaceOverlay.topvalue==-1){
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-//    public void detectFaces(com.google.android.gms.vision.Frame frame)
-//    {
-//        com.google.android.gms.vision.face.FaceDetector faceDetector = new
-//                com.google.android.gms.vision.face.FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
-//                .build();
-//
-//        if(!faceDetector.isOperational()) {
-//            //Toast.makeText(getApplicationContext(),"Failed to get face",Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//
-//        SparseArray<com.google.android.gms.vision.face.Face> faces = faceDetector.detect(frame);
-//        Log.e("Faces Frame",faces.size()+"");
-//
-//
-//        for (int i = 0; i < faces.size(); i++) {
-//            Face thisFace = faces.valueAt(i);
-//            float x1 = thisFace.getPosition().x;
-//            float y1 = thisFace.getPosition().y;
-//            float x2 = x1 + thisFace.getWidth();
-//            float y2 = y1 + thisFace.getHeight();
-//            //tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
-//            //Toast.makeText(getApplicationContext(),"Failed to get face",Toast.LENGTH_SHORT).show();
-//
-//        }
-//    }
-    public static Point getLocationOnScreen(View view) {
-        int[] location = new int[2];
-        view.getLocationOnScreen(location);
-        return new Point(location[0], location[1]);
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams)captured.getLayoutParams();
+                        params.setMargins(params.leftMargin, (int) (FaceOverlay.topvalue+(params.height/2)),params.rightMargin,params.bottomMargin);
+                        captured.setLayoutParams(params);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    void onFaceScanned(){
+        captured.setVisibility(View.VISIBLE);
+        captured.loop(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },1000);
+
+
     }
 }
